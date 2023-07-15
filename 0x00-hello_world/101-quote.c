dfe844548b18b97e1310860d2ca02854c817fbf0
Gugulethu <gugulethumzamani@gmail.com>
2023-07-15 16:49:32 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error code)
+ */
+int main(void)
+{
+int len;
+char *str;
+
+str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
+
+/* write() function writes to the standard error */
+write(2, str, len);
+
+return (1);
+}
