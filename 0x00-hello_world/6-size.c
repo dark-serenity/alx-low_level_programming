c1c26fdb9f1cdbe3b34cf242233fb6988acfbec0
Gugulethu <gugulethumzamani@gmail.com>
2023-07-15 15:52:18 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program that prints values
+ * return: 0 (success)
+ */
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", size of(char));
+printf("Size of an int: %lu byte(s)\n", size of(int));
+printf("Size of a long int: %lu byte(s)\n", size of(long int));
+printf("Size of a long long int: %lu byte(s)\n", size of(long long int));
+printf("Size of a float: %lu byte(s)\n", size of(float));
+return (0);
+}
