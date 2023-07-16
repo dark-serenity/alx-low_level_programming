658c32d3840e298706cc839d85b42f9ea670155b
Gugulethu <gugulethumzamani@gmail.com>
2023-07-16 04:49:07 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,28 +6,37 @@
  * Description: Prints all possible combinations of two two-digit numbers,
  *              meeting the given requirements, using putchar function.
  *
- * Return: (0) Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int num1, num2;
+	int num1, num2, num3, num4;
 
-	for (num1 = 0; num1 < 100; num1++)
+	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 = 0; num2 < 100; num2++)
+		for (num2 = 0; num2 < 10; num2++)
 		{
-			if (num1 <= num2)
+			for (num3 = 0; num3 < 10; num3++)
 			{
-				putchar((num1 / 10) + '0');
-				putchar((num1 % 10) + '0');
-				putchar(' ');
-				putchar((num2 / 10) + '0');
-				putchar((num2 % 10) + '0');
-
-				if (num1 != 99 || num2 != 99)
+				for (num4 = 0; num4 < 10; num4++)
 				{
-					putchar(',');
-					putchar(' ');
+					int firstNum = num1 * 10 + num2;
+					int secondNum = num3 * 10 + num4;
+
+					if (firstNum < secondNum)
+					{
+						putchar(num1 + '0');
+						putchar(num2 + '0');
+						putchar(' ');
+						putchar(num3 + '0');
+						putchar(num4 + '0');
+
+						if (firstNum != 98 || secondNum != 99)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
 				}
 			}
 		}
