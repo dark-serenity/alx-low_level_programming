2175f691191bc4842229d3a537a92f612fa53c18
Gugulethu <gugulethumzamani@gmail.com>
2023-07-16 02:40:26 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,24 +11,24 @@
  */
 int main(void)
 {
-	int n;
+    int n;
 
-	/* Seed the random number generator */
-	srand(time(0));
+    /* Seed the random number generator */
+    srand(time(0));
 
-	/* Generate a random number */
-	n = rand();
+    /* Generate a random number */
+    n = rand();
 
-	printf("Last digit of %d is %d", n, n % 10);
+    printf("Last digit of %d is %d", n, abs(n % 10));
 
-	if (n % 10 > 5)
-		printf(" and is greater than 5\n");
-	else if (n % 10 == 0)
-		printf(" and is 0\n");
-	else
-		printf(" and is less than 6 and not 0\n");
+    if (abs(n % 10) > 5)
+        printf(" and is greater than 5\n");
+    else if (abs(n % 10) == 0)
+        printf(" and is 0\n");
+    else
+        printf(" and is less than 6 and not 0\n");
 
-	return (0);
+    return 0;
 }
 
 
