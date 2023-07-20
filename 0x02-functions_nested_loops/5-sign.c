6ba3df840bfc97b5adf59ded3f389a7da438869e
Gugulethu <gugulethumzamani@gmail.com>
2023-07-19 17:21:29 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,22 +10,22 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n == 0)
+    {
+        _putchar('0');
+        return (0);
+    }
+    else if (n < 0)
+    {
+        _putchar('-');
+        return (-1);
+    }
 
-	// Handle invalid input
-	return (-2);
+    /* Handle invalid input */
+    return (-2);
 }
