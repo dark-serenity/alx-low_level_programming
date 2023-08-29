934af8e8fe3f5c875f3a9d4e39f2c3e92e5df81c
Gugulethu <gugulethumzamani@gmail.com>
2023-08-29 16:29:20 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,21 +2,46 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - Returns the nth node of a listint_t linked list.
- * @head: Pointer to the head of the list.
- * @index: Index of the node to retrieve.
- * Return: Address of the node at the specified index, or NULL if not found.
+ * insert_nodeint_at_index - Inserts a new node at a given position.
+ * @head: Pointer to a pointer to the head of the list.
+ * @idx: Index at which the new node should be inserted.
+ * @n: Value to be assigned to the new node.
+ * Return: Address of the new node, or NULL if it failed.
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int count = 0;
+    unsigned int count = 0;
+    listint_t *new_node = NULL;
+    listint_t *current = *head;
 
-while (head != NULL)
-{
-if (count == index)
-return (head);
+    if (head == NULL)
+        return (NULL);
 
-count++;
-head = head->next;
-return (NULL);
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->n = n;
+
+    if (idx == 0)
+    {
+        new_node->next = *head;
+        *head = new_node;
+        return (new_node);
+    }
+
+    while (current != NULL)
+    {
+        if (count == idx - 1)
+        {
+            new_node->next = current->next;
+            current->next = new_node;
+            return (new_node);
+        }
+        count++;
+        current = current->next;
+    }
+
+    free(new_node);
+    return (NULL);
 }
