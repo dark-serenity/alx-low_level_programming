3eeae5058a2b7fd2c35c79e9c33477357a3097e1
Gugulethu <gugulethumzamani@gmail.com>
2023-08-29 16:39:04 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,38 +10,38 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    unsigned int count = 0;
-    listint_t *new_node = NULL;
-    listint_t *current = *head;
+unsigned int count = 0;
+listint_t *new_node = NULL;
+listint_t *current = *head;
 
-    if (head == NULL)
-        return (NULL);
+if (head == NULL)
+return (NULL);
 
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
 
-    new_node->n = n;
+new_node->n = n;
 
-    if (idx == 0)
-    {
-        new_node->next = *head;
-        *head = new_node;
-        return (new_node);
-    }
+if (idx == 0)
+{
+new_node->next = *head;
+*head = new_node;
+return (new_node);
+}
 
-    while (current != NULL)
-    {
-        if (count == idx - 1)
-        {
-            new_node->next = current->next;
-            current->next = new_node;
-            return (new_node);
-        }
-        count++;
-        current = current->next;
-    }
+while (current != NULL)
+{
+if (count == idx - 1)
+{
+new_node->next = current->next;
+current->next = new_node;
+return (new_node);
+}
+count++;
+current = current->next;
+}
 
-    free(new_node);
-    return (NULL);
+free(new_node);
+return (NULL);
 }
