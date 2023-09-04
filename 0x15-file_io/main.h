04b217df31cd27b6a0da50ee67300177352fbcb9
Gugulethu <gugulethumzamani@gmail.com>
2023-09-04 16:13:58 -0700

    third commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,22 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-/* Task 0: Tread lightly, she is near */
 ssize_t read_textfile(const char *filename, size_t letters);
-
-/* Task 1: Under the snow */
 int create_file(const char *filename, char *text_content);
-
-/* Task 2: Speak gently, she can hear */
 int append_text_to_file(const char *filename, char *text_content);
 
-/* Task 3: cp */
-void error_message(const char *msg, int code);
-
-/* Task 4: elf (Advanced) */
-void error_exit(const char *msg, int code);
-void print_elf_header(Elf64_Ehdr *ehdr);
-
-#endif /* MAIN_H */
+#endif
