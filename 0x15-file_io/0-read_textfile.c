9fda1660dd3a6c2dc39f7537b7c2e93660b96288
Gugulethu <gugulethumzamani@gmail.com>
2023-09-04 15:58:51 -0700

    textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,6 +4,14 @@
 #include <fcntl.h>
 #include "main.h"
 
+/**
+ * read_textfile - Reads a text file and prints it.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed. If an error occurs,
+ * returns 0.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 int fd;
@@ -42,6 +50,5 @@ return (0);
 
 free(buffer);
 close(fd);
-return (bytes_written);
+return (bytes_read);
 }
-
